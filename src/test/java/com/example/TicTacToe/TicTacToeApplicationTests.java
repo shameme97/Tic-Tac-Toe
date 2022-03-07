ef8812dbcf28a7041cb0ff3b84d069ce8293fbf5
shameme97 <samihashameem@gmail.com>
2022-03-08 00:17:54 +0600

    Add test directory

---
+++ src/test/java/com/example/TicTacToe/TicTacToeApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.TicTacToe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}

54c7c9e405004c8f949bd5a14a0e860f12e1f9cf
shameme97 <samihashameem@gmail.com>
2022-03-09 02:42:03 +0600

    Modify model

--- src/main/java/com/example/TicTacToe/Model/Board.java
+++ src/main/java/com/example/TicTacToe/Model/Board.java
@@ -14,21 +14,8 @@ public class Board {
     @Id
     private String id;
     private int size;
-//    private List<String[]> winStates_horizontal;
-//    private List<String[]> winStates_vertical;
-//    private List<String[]> winStates_rightDiagonal;
-//    private List<String[]> winStates_leftDiagonal;
     private List<String[]> winStates;
     private String winner;
-
-//    public Board(int size, List<String[]> winStates_horizontal, List<String[]> winStates_vertical,
-//                 List<String[]> winStates_rightDiagonal, List<String[]> winStates_leftDiagonal){
-//        this.size = size;
-//        this.winStates_horizontal = winStates_horizontal;
-//        this.winStates_vertical = winStates_vertical;
-//        this.winStates_rightDiagonal = winStates_rightDiagonal;
-//        this.winStates_leftDiagonal = winStates_leftDiagonal;
-//    }
 
     public Board(int size, List<String[]> winStates){
         this.size = size;

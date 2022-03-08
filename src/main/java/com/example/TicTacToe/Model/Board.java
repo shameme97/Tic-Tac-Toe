0736e0bb18dab7ceb765d4d8781bf8ddc8eb7d29
shameme97 <samihashameem@gmail.com>
2022-03-09 01:11:12 +0600

    Configure win states

--- src/main/java/com/example/TicTacToe/Model/Board.java
+++ src/main/java/com/example/TicTacToe/Model/Board.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @Document(collection = "Board")
@@ -12,9 +14,18 @@ public class Board {
     @Id
     private String id;
     private int size;
-    private String[] winStates_horizontal;
-    private String[] winStates_vertical;
-    private String[] winStates_rightDiagonal;
-    private String[] winStates_leftDiagonal;
+    private List<String[]> winStates_horizontal;
+    private List<String[]> winStates_vertical;
+    private List<String[]> winStates_rightDiagonal;
+    private List<String[]> winStates_leftDiagonal;
     private String winner;
+
+    public Board(int size, List<String[]> winStates_horizontal, List<String[]> winStates_vertical,
+                 List<String[]> winStates_rightDiagonal, List<String[]> winStates_leftDiagonal){
+        this.size = size;
+        this.winStates_horizontal = winStates_horizontal;
+        this.winStates_vertical = winStates_vertical;
+        this.winStates_rightDiagonal = winStates_rightDiagonal;
+        this.winStates_leftDiagonal = winStates_leftDiagonal;
+    }
 }
--- src/main/java/com/example/TicTacToe/Service/GameServiceImpl.java
+++ src/main/java/com/example/TicTacToe/Service/GameServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.TicTacToe.Service;
 
+import com.example.TicTacToe.Model.Board;
 import com.example.TicTacToe.Repository.GameRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,67 @@ public class GameServiceImpl implements GameService {
     @Override
     public void beginGame(int size) {
         // create board object with size win_states
+        List<String[]> winStates_horizontal = new ArrayList<>();
+        List<String[]> winStates_vertical = new ArrayList<>();
+        List<String[]> winStates_rightDiagonal = new ArrayList<>();
+        List<String[]> winStates_leftDiagonal = new ArrayList<>();
+        int row, col;
+        String[] populate;
+        for (int i=0; i<size; ++i){
+            populate = new String[size];
+            row = i;
+            col = 0;
+            for (int j=0; j<size; ++j){
+                populate[j] = row+""+col;
+                ++col;
+            }
+            winStates_horizontal.add(populate);
+        }
+
+        for (int i=0; i<size; ++i){
+            populate = new String[size];
+            row = 0;
+            col = i;
+            for (int j=0; j<size; ++j){
+                populate[j] = row+""+col;
+                ++row;
+            }
+            winStates_vertical.add(populate);
+        }
+        String[] populate2;
+        int len = (size-3)*2 + 1;
+        for (int i=0; i<len; ++i){
+            populate = new String[len];
+            populate2 = new String[len];
+            row = 0;
+            col = i;
+            for (int j=0; j<len; ++j){
+                populate[j] = row+""+col;
+                populate2[j] = row+""+(len-1-col);
+                ++row;
+                ++col;
+            }
+            --len;
+            winStates_rightDiagonal.add(populate);
+            winStates_leftDiagonal.add(populate2);
+        }
+        len = (size-3)*2 + 1;
+        for (int i=1; i<len; ++i){
+            populate = new String[len];
+            populate2 = new String[len];
+            row = i;
+            col = 0;
+            for (int j=0; j<len; ++j){
+                populate[j] = row+""+col;
+                populate2[j] = row+""+(len-1-col);
+                ++row;
+                ++col;
+            }
+            --len;
+            winStates_rightDiagonal.add(populate);
+            winStates_leftDiagonal.add(populate2);
+        }
+
     }
 
     @Override
@@ -45,4 +107,6 @@ public class GameServiceImpl implements GameService {
     public void increaseWin(String player) {
         // increment win of player by 1
     }
+
+
 }

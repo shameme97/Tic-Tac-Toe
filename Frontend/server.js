e9759d2443eb7d6016b804d690baac005a10937e
shameme97 <samihashameem@gmail.com>
2022-03-18 16:24:07 +0600

    Modify for heroku deployment

--- Frontend/server.js
+++ Frontend/server.js
@@ -6,3 +6,19 @@ app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 8080;
 app.listen(port);
 console.log('server started '+ port);
+
+// const http = require('http');
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// app.use(express.json());
+// app.use(express.static("src"));
+
+// app.use('/', function(req,res){
+//     res.sendFile(path.join(__dirname+'/dist'));
+//   });
+// const server = http.createServer(app);
+
+// var port = process.env.PORT || 8080;
+// app.listen(port);
+// console.log('server started '+ port);

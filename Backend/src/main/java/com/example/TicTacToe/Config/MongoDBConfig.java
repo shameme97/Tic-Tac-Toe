2431c5f9b0856c030ec4d3a36b22d7a38f3cbcc3
shameme97 <samihashameem@gmail.com>
2022-03-19 01:59:46 +0600

    Add MongoDB config file

---
+++ Backend/src/main/java/com/example/TicTacToe/Config/MongoDBConfig.java
@@ -0,0 +1,31 @@
+package com.example.TicTacToe.Config;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
+public class MongoDBConfig
+{
+    @Bean
+    public MongoClient mongo()
+    {
+        //ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/TicTacToeDB");
+        ConnectionString connectionString = new ConnectionString("mongodb+srv://samiha_shameme:a1b2c3@cluster0.ednu7.mongodb.net/TicTacToeDB");
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+
+        return MongoClients.create(mongoClientSettings);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() throws Exception
+    {
+        return new MongoTemplate(mongo(), "TicTacToeDB");
+    }
+}
